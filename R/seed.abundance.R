8044aa05d575a9675bffea47d22d09e68c769d01
PalaeoTom <fo18751@bristol.ac.uk>
2023-03-09 17:05:08 +0000

    base seeding functions added

---
+++ R/seed.abundance.R
@@ -0,0 +1,22 @@
+#' Default method of assigning starting abundance values to populations
+#'
+#' Values are drawn from a Weibull distribution fitted to the data of Traill et al. Minimum value returned is 10 (the minimum estimate derived from the data of Traill et al.).
+#'
+#' @param minimum a numeric value specifying the minimum acceptable abundance number. Default is 10.
+#'
+#' @return a numeric value.
+#' @export
+#'
+#' @keywords internal
+#' @references Traill, L. W., Bradshaw, C. J. A., & Brook, B. W. (2007). Minimum viable population size: A meta-analysis of 30 years of published estimates. \emph{Biological Conservation}, 139, 156-166.
+#'
+#' @examples
+#' # Run the function
+#' seed.abundance()
+seed.abundance <- function(minimum = 10){
+  while(T){
+    A <- round(rweibull(1, scale = 2723.498572, shape = 0.54345227), digits = 0)
+    if(A >= minimum) break()
+  }
+  return(A)
+}
---
+++ R/seed.gen.het.R
@@ -0,0 +1,21 @@
+#' Default method of assigning starting genetic heterogeneity values to populations
+#'
+#' Values are drawn from a uniform distribution bounded between 0 and 1. Only produces nonzero values by default.
+#'
+#' @param cutoff a numeric value specifying the largest gen.het value that will be re-sampled. Default is 0.
+#'
+#' @return a numeric value.
+#' @export
+#'
+#' @keywords internal
+#'
+#' @examples
+#' # Run the function
+#' seed.gen.het()
+seed.gen.het <- function(cutoff = 0){
+  while(T){
+    GH <- runif(1, 0, 1)
+    if(GH > cutoff) break()
+  }
+  return(GH)
+}
