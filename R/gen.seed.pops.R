a6eee1f0982b848564d234ffc796d0229a7abf82
PalaeoTom <fo18751@bristol.ac.uk>
2023-03-21 12:37:50 +0000

    work in progress gen.seed.pops

--- R/gen.seed.pops.R
+++ R/gen.seed.pops.R
@@ -1,23 +1,33 @@
-n = 10
 
 gen.seed.pops <- function(stage, pop.var.seeds, n, method = "random", export = F, name.out = "new"){
   ## check stage is a stage object
-
+  if(!class(stage)=="stage"){
+    stop("stage is not a stage object")
+  }
   ## check pop.var.seeds is a pop.var.seeds object
-
+  if(!class(pop.var.seeds)=="pop.var.seeds"){
+    stop("pop.var.seeds is not a pop.var.seeds object")
+  }
   ## check n is numeric
-
+  if(!is.numeric(n)){
+    stop("n is not numeric")
+  }
   ## check method is "random" or a function
-
+  if(!method == "random" && !is.function(method)){
+    stop("provided seeding method is not 'random' or a function")
+  }
   ## get regions to be populated (vector of numbers - each entry corresponds to a population)
-
-  ## check regions to be populated is a vector of numbers
+  rtbp <- sample(as.vector(stage[[1]]), n, replace = T)
 
   ## initialise list of regions (length = number of cells in stage regions object)
+  occ.reg <- as.list(matrix(nrow = length(stage[[1]])))
 
   ## initialise list of seed populations (length = n)
+  seed.pops <- as.list(matrix(nrow = n))
 
   ## for each entry in the vector: 1) add position of entry in vector to list of regions; 2) add list of population variables to seed population list.
+  unlist(lapply(1:length(pop.var.seeds), function(x) pop.var.seeds[[x]]()))
+
 
   ## return pair of lists (1 - occupied regions, 2 - populations) as seeds object
 
