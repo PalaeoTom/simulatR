62e2f1911fe21b77443be9fedf8740a6b0d030aa
PalaeoTom <fo18751@bristol.ac.uk>
2023-07-03 19:00:19 +0100

    Finished model.genome. Fixed model.abundance.

--- R/model.abundance.R
+++ R/model.abundance.R
@@ -12,50 +12,73 @@
 #' @export
 #'
 #' @keywords internal
+#' #' # make a stage
+#' s <- make.stage(n.col = 5, n.row = 5, ar = 400)
+#'
+#' # define population variables
+#' PVs <- set.pop.var.seeds(pop.gen.threshold = 0.1, new.var.name = "PV1", new.var.seed = function() runif(1,10,50))
+#'
+#' # generate seed populations
+#' p0 <- gen.seed.pops(stage = s, pop.var.seeds = PVs, n = 10)
+#'
+#' # Create vector specifying times
+#' t.series <- c(100,90,80,70,60,50,40,30,20,10)
+#'
+#' model.abundance(p0 = p0, p.ID = sample(1:length(p0$population.variables), 1), t0 = 100, t.series = t.series)
 model.abundance <- function(p0, p.ID, t.series, t0, SF = 1){
   ## get new time
   t1 <- t.series[which(t.series == t0)+1]
   ## get abundance at previous time step
-  a0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "abundance")]
+  a0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "abundance")])
   ## scale SD by time elapsed and factor
   scale.F <- abs((t1-t0)*SF)
   ## if pop.gen switched on
   if(any(p0$variable.names == "PGT")){
     ## get rate
-    PGR0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "PGR")]
+    PGR0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "PGR")])
     ## get threshold
-    PGT0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "PGT")]
+    PGT0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "PGT")])
     if(PGR0 > PGT0){
       ## If min.via.pop provided, determine whether abundance is
       if(any(p0$variable.names == "MVP")){
         ## get MVP
-        MVP0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "MVP")]
+        MVP0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "MVP")])
         ## if a0 => MVP0
         if(a0 >= MVP0){
           a1 <- a0 + round(rnorm(1, mean = a0*(PGR0-PGT0), sd = scale.F), digits = 0)
         } else {
           a1 <- a0 + round(rnorm(1, mean = a0-(MVP0+(a0*(PGR0-PGT0))), sd = scale.F), digits = 0)
         }
+        ## if a1 is negative, change to 0
+        if(a1 < 1) a1 <- 0
         ## return a1
         return(a1)
       } else {
         a1 <- a0 + round(rnorm(1, mean = a0*(PGR0-PGT0), sd = scale.F), digits = 0)
+        ## if a1 is negative, change to 0
+        if(a1 < 1) a1 <- 0
         ## return a1
         return(a1)
       }
     } else {
       ## If min.via.pop provided, determine whether abundance is
       if(any(p0$variable.names == "MVP")){
+        ## get MVP
+        MVP0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "MVP")])
         ## if a0 => MVP0
         if(a0 >= MVP0){
           a1 <- a0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
         } else {
           a1 <- a0 + round(rnorm(1, mean = a0-MVP0, sd = scale.F), digits = 0)
         }
+        ## if a1 is negative, change to 0
+        if(a1 < 1) a1 <- 0
         ## return a1
         return(a1)
       } else {
         a1 <- a0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
+        ## if a1 is negative, change to 0
+        if(a1 < 1) a1 <- 0
         ## return a1
         return(a1)
       }
@@ -63,16 +86,22 @@ model.abundance <- function(p0, p.ID, t.series, t0, SF = 1){
   } else {
     ## If min.via.pop provided, determine whether abundance is
     if(any(p0$variable.names == "MVP")){
+      ## get MVP
+      MVP0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "MVP")])
       ## if a0 => MVP0
       if(a0 >= MVP0){
         a1 <- a0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
       } else {
         a1 <- a0 + round(rnorm(1, mean = a0-MVP0, sd = scale.F), digits = 0)
       }
+      ## if a1 is negative, change to 0
+      if(a1 < 1) a1 <- 0
       ## return a1
       return(a1)
     } else {
       a1 <- a0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
+      ## if a1 is negative, change to 0
+      if(a1 < 1) a1 <- 0
       ## return a1
       return(a1)
     }
--- R/model.genome.R
+++ R/model.genome.R
@@ -13,13 +13,24 @@
 #' @export
 #'
 #' @keywords internal
+#' # make a stage
+#' s <- make.stage(n.col = 5, n.row = 5, ar = 400)
+#'
+#' # define population variables
+#' PVs <- set.pop.var.seeds(pop.gen.threshold = 0.1, new.var.name = "PV1", new.var.seed = function() runif(1,10,50))
+#'
+#' # generate seed populations
+#' p0 <- gen.seed.pops(stage = s, pop.var.seeds = PVs, n = 10)
+#'
+#' # Create vector specifying times
+#' t.series <- c(100,90,80,70,60,50,40,30,20,10)
+#'
+#' model.genome(p0 = p0, p.ID = sample(1:length(p0$population.variables), 1), t.series = t.series, t0 = 100)
 model.genome <- function(p0, p.ID, t.series, t0, SF = 1){
   ## get new time
   t1 <- t.series[which(t.series == t0)+1]
   ## get genome at previous time step
-  g0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "G")]
-  ## scale SD by time elapsed and factor
-  scale.F <- (t0-t1)*SF
+  g0 <- unname(p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "G")])
   ## if pop.gen switched on
   if(any(p0$variable.names == "PGT")){
     ## identify species
@@ -34,19 +45,19 @@ model.genome <- function(p0, p.ID, t.series, t0, SF = 1){
       ## find location of other population
       r1 <- which(sapply(1:length(p0$populated.regions), function(x) any(p0$populated.regions[[x]] == i)))
       ## get average dispersal distance of population
-      ADD1 <- p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "ADD")]
+      ADD1 <- unname(p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "ADD")])
       ## is distance equal to or than average dispersal distance multiplied by time
       if(p0$distances[r,r1] <= ADD1*(t0-t1)){
         ## find abundance of considered population
-        a1 <- (p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "abundance")])
+        a1 <- unname((p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "abundance")]))
         ## find disp.prop of considered population
-        DP1 <- p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "DP")]
+        DP1 <- unname(p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "DP")])
         ## find genome of considered population
-        g1 <- p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "G")]
+        g1 <- unname(p0$population.variables[[i]][which(names(p0$population.variables[[i]]) == "G")])
         ## calculate weighted abundance
-        wa1 <- unname((((ADD1*(t0-t1))-p0$distances[r,r1])/(ADD1*(t0-t1)))*DP1*a1)
+        wa1 <- (((ADD1*(t0-t1))-p0$distances[r,r1])/(ADD1*(t0-t1)))*DP1*a1
         ## calculate wg value
-        wg1 <- unname(wa1*g1)
+        wg1 <- wa1*g1
         ## add to numerators and denominators
         num <- c(num, wg1)
         den <- c(den, wa1)
@@ -58,16 +69,27 @@ model.genome <- function(p0, p.ID, t.series, t0, SF = 1){
     ## get abundance of p.ID
     a0 <- p0$population.variables[[p.ID]][which(names(p0$population.variables[[p.ID]]) == "abundance")]
     ## calculate weighted genome for species
-    wg <- sum(num+(a0*g0))/sum(den+a0)
-    ## get rate
-
-
-
-    g1 <- g0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
+    if(is.null(num)){
+      wg <- g0
+    } else {
+      wg <- sum(num+(a0*g0))/sum(den+a0)
+    }
+    ## log(a0) and log(rate have an approximately linear relationship). Use y = mc+c to get rate.
+    rate <- 2.718281828^((-0.61258200*log(a0))-11.64115066)
+    ## calculate change
+    while(TRUE){
+      change <- rnorm(1, mean = (wg-g0), sd = rate*(t0-t1)*SF)
+      if(change <= (1-g0) && change >= (0-g0)) break()
+    }
+    g1 <- g0 + change
     ## return g1
     return(g1)
   } else {
-    g1 <- g0 + round(rnorm(1, mean = 0, sd = scale.F), digits = 0)
+    while(TRUE){
+      change <- rnorm(1, mean = 0, sd = (t0-t1)*SF)
+      if(change <= (1-g0) && change >= (0-g0)) break()
+    }
+    g1 <- g0 + change
     ## return g1
     return(g1)
   }
