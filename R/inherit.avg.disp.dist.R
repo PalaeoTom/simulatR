0c787bf0c47a6f2657d3fc5614c428d1648a0e1e
PalaeoTom <fo18751@bristol.ac.uk>
2023-07-04 18:51:22 +0100

    added inherit.avg.disp.dist

--- R/inherit.avg.disp.dist.R
+++ R/inherit.avg.disp.dist.R
@@ -1,12 +1,12 @@
-#' Minimum viable population number inheritance model
+#' Average dispersal distance inheritance model
 #'
-#' Changes to MVP that occur during speciation event. Tied to abundance of population (large population = broad distribution new MVP can be sampled from.
+#' Changes to ADD that occur during speciation event. Tied to abundance of population (large population = broad distribution new ADD can be sampled from.
 #'
 #' @param p0 populations object from previous time bin.
 #' @param p the numeric index of the population in p0$population.variables.
 #' @param t0 time of previous time bin.
 #' @param t1 time of current time bin.
-#' @param SF a numeric value by which the changes in MVP at speciation event can be scaled. For flexibility.
+#' @param SF a numeric value by which the changes in ADD at speciation event can be scaled. For flexibility.
 #'
 #' @return a numeric value.
 #' @export
@@ -22,50 +22,49 @@
 #' p0 <- gen.seed.pops(stage = s, pop.var.seeds = PVs, n = 10)
 #'
 #' # Run function
-#' inherit.min.via.pop(p0 = p0, p = sample(1:length(p0$population.variables), 1), t0 = 100, t1 = 90)
-inherit.min.via.pop <- function(p0, p, t0, t1, min = 1, max = NULL, SF = 1){
-  ## get MVP at previous time step
-  MVP0 <- unname(p0$population.variables[[p]][which(names(p0$population.variables[[p]]) == "MVP")])
+#' inherit.avg.disp.dist(p0 = p0, p = sample(1:length(p0$population.variables), 1), t0 = 100, t1 = 90)
+inherit.avg.disp.dist <- function(p0, p, t0, t1, min = 0.005, max = NULL, SF = 1){
+  ## get ADD at previous time step
+  ADD0 <- unname(p0$population.variables[[p]][which(names(p0$population.variables[[p]]) == "ADD")])
   ## if pop.gen switched on
   if(any(p0$variable.names == "PGT")){
     ## get abundance
     A0 <- unname(p0$population.variables[[p]][which(names(p0$population.variables[[p]]) == "A")])
     ## get value between min and max
     while(TRUE){
-      ## sample new value - normal distribution with mean MVP0 and SD determine by time elapsed, SF, and number of times abundance goes into minimum viable population number
-      MVP1 <- round(rnorm(1, mean = MVP0, sd = abs(t1-t0)*SF*(A0/MVP0)), digits = 0)
+      ADD1 <- round(rnorm(1, mean = ADD0, sd = abs(t1-t0)*SF*(A0/ADD0)), digits = 0)
       ## If min and max employed
       if(is.numeric(min) && is.numeric(max)){
-        if(MVP1 >= min && MVP1 <= max) break()
+        if(ADD1 >= min && ADD1 <= max) break()
       }
       ## If just min
       if(is.numeric(min) && is.null(max)){
-        if(MVP1 >= min) break()
+        if(ADD1 >= min) break()
       }
       ## If just max
       if(is.null(min) && is.numeric(max)){
-        if(MVP1 <= max) break()
+        if(ADD1 <= max) break()
       }
     }
   } else {
     ## get value between min and max
     while(TRUE){
-      ## sample new value - normal distribution with mean MVP0 and SD determine by time elapsed and SF alone.
-      MVP1 <- round(rnorm(1, mean = MVP0, sd = abs(t1-t0)*SF), digits = 0)
+      ## sample new value - normal distribution with mean ADD0 and SD determine by time elapsed and SF alone.
+      ADD1 <- round(rnorm(1, mean = ADD0, sd = abs(t1-t0)*SF), digits = 0)
       ## If min and max employed
       if(is.numeric(min) && is.numeric(max)){
-        if(MVP1 >= min && MVP1 <= max) break()
+        if(ADD1 >= min && ADD1 <= max) break()
       }
       ## If just min
       if(is.numeric(min) && is.null(max)){
-        if(MVP1 >= min) break()
+        if(ADD1 >= min) break()
       }
       ## If just max
       if(is.null(min) && is.numeric(max)){
-        if(MVP1 <= max) break()
+        if(ADD1 <= max) break()
       }
     }
   }
-  return(MVP1)
+  return(ADD1)
 }
 
